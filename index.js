8c1c624535e1cfef577436962f80f3a25b494e5d
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-03-10 17:46:24 -0600

    test: testing to routes

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello world');
+    // res.render()
+    // res.json
+    // res.status()
+});
+
+app.get('/us', (req, res) => {
+    res.send('Us');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Contact');
+});
+
 app.listen(port, () => {
     console.log(`The server is running on port ${port}`);
 });
