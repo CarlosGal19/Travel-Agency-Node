d0a3bc523d57428c11f6c43be6cfddcb9d36d419
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-03-17 13:08:19 -0600

    feat: footer created

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-// Add router
-app.use('/', router);
-
 // Enable pug
 app.set('view engine', 'pug');
 
+// Get current year with middleware
+app.use((req, res, next) => {
+    // Locals are local variable from express that are availables in the views
+    const year = new Date();
+    console.log(year.getFullYear());
+    res.locals.currentYear = year.getFullYear();
+    return next();
+});
+
 // Define public folder
 app.use(express.static('public'));
+
+// Add router
+app.use('/', router);
 
 app.listen(port, () => {
     console.log(`The server is running on port ${port}`);
