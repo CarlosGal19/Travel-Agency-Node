94e43f14d7df9a8fdb1557e0c3aa72acf2342ddd
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-03-18 16:33:26 -0600

    feat: database is created and db.js takes into to realize the connection

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+const db = new Sequelize(process.env.DB_URL, {
     dialect: 'mysql',
     port: 3306,
     define: {
